c49fd5b594ffbb7fd89ac093afdbcfa1deb84d2f
vvitoriaddanielads13sp <vitoriadaniela97@gmail.com>
2022-03-15 21:32:16 -0300

    first commit

--- AluraImersaoDev/Mentalista/app.js
+++ AluraImersaoDev/Mentalista/app.js
@@ -1,35 +1,24 @@
 var numSecreto = parseInt(Math.random()*11);
+console.log("nSecreto: " + numSecreto);
+
 var chances = 3;
 console.log("score: "+ chances);
 
 function Chutar(){
-    
     var elemento = parseInt(document.getElementById("valor").value);
     console.log("elemento: " +elemento);
-        
-    console.log("nSecreto: " + numSecreto);
     var resultado = document.getElementById("resultado");
-    while(chances<=3){
-        if(chances > 0){
-            if(elemento == numSecreto){
-                resultado.innerHTML = "MENTALIZEI!" + " <p>" + " Você Acertou";               
-                chances-=1;
-                
-                console.log("score "+chances);
-            }
-            else
-                {
-                if(elemento < numSecreto )
-                {        
-                    chances-=1;
-                  resultado.innerHTML = "O NÚMERO SECRETO É MAIOR";
-                  
+        
+        if(elemento == numSecreto){
+            resultado.innerHTML = "MENTALIZEI!" + " <p>" + " Você Acertou" + "<p> TENTATIVAS: " + chances;               
+                }else if(elemento < numSecreto ){        
+                    resultado.innerHTML = "O NÚMERO SECRETO É MAIOR" + "<p> TENTATIVAS: " + chances;                  
                  }else if(elemento>numSecreto){
-                    resultado.innerHTML = "O NÚMERO SECRETO É MENOR";
-                    
-                 }         
+                    resultado.innerHTML = "O NÚMERO SECRETO É MENOR" + "<p> TENTATIVAS: " + chances;                    
+                 }    
+                 chances--;  
+                 if(chances == 0){
+                    window.alert("FIM DE JOGO :( APERTE F5 PARA COMEÇAR NOVAMENTE!");                    
+                    resultado.innerHTML = "TENTATIVAS: " + chances;
+                 } 
             }
-        }    
-    }
-    
-}
